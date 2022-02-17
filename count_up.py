adb84a955938a2d1b9dfa3fbd9b408340493a83a
Dania Irshad <daniairshad@live.com>
2022-02-17 17:47:21 -0600

    complete count_up function

--- count_up.py
+++ count_up.py
@@ -11,8 +11,9 @@ def count_up(start, stop):
         6
         7
     """
-
-    # YOUR CODE HERE
-
+    count = start
+    while count <= stop:
+        print(count)
+        count += 1
 
 count_up(5, 7)        
