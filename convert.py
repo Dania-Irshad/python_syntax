b4d1e34fab3f023dd23e1055a84afa76d4e21777
Dania Irshad <daniairshad@live.com>
2022-02-17 18:44:25 -0600

    completed convert_temp function

--- convert.py
+++ convert.py
@@ -14,9 +14,18 @@ def convert_temp(unit_in, unit_out, temp):
       convert_temp("c", "f", 0)  =>  32.0
       convert_temp("f", "c", 212) => 100.0
     """
-
-    # YOUR CODE HERE
-
+    converted_temp = 0
+    if unit_in == "f" and unit_out == "c":
+      converted_temp = (temp - 32) * (5/9)
+    elif unit_in == "c" and unit_out == "f":
+      converted_temp = (temp * 9/5) + 32
+    elif unit_in == unit_out:
+      return temp
+    elif unit_in != "f" and unit_in != "c":
+      return f"Invalid unit {unit_in}"
+    elif unit_out != "f" and unit_out != "c":
+      return f"Invalid unit {unit_out}"
+    return converted_temp
 
 print("c", "f", 0, convert_temp("c", "f", 0), "should be 32.0")
 print("f", "c", 212, convert_temp("f", "c", 212), "should be 100.0")
